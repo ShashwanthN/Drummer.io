70fc6cd654432d67bc308f8bb3ea4d2431d936ac
shashwanth <shashwanthsivakumarn@gmail.com>
2024-02-08 17:39:56 +0530

    adding files and making style changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { CiDark } from "react-icons/ci";
 import { CiLight } from "react-icons/ci";
 import drum from "./images/drum.png";
+import { TopBar } from './components';
+import kick from "./images/kick.png";
 
 function App() {
   const { theme } = useSelector((state) => state.theme);
@@ -15,13 +17,16 @@ function App() {
 
   return (
     <div data-theme={theme} className='w-full min-h-[100vh] overflow-y-scroll'>
+      <TopBar/>
       <div className="bg-bg w-full h-screen  justify-center items-center">
         <div className='home w-full px-4 2xl:px-0 bg-bgColor h-full'>
           <div className='w-full flex flex-row lg:flex-row justify-center pb-10 h-full'>
             {/* Left */}
             <div className="text-bg2 flex flex-col justify-center items-center lg:items-start lg:w-1/4">
               <button className="q p-3 items-center hover:shadow-lg drum">1</button>
-              <button className="w drum">2</button>
+              <button className="w drum">
+  <img src={kick} alt="icon of kicking something"></img>
+</button>
               <button className="e drum">3</button>
             </div>
 
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+
+import TopBar from "./TopBar";
+
+export {TopBar};
+
